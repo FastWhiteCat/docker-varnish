4a986d1fe6dcc05b6762e2704b1a87d56bc4bcff
Maciej <maciej.wieczorek@fastwhitecat.com>
2017-06-14 12:51:20 +0200

    Set storage to file

--- start.sh
+++ start.sh
@@ -3,10 +3,21 @@
 CONF_FILE="/etc/varnish/varnish.vcl"
 SECRET_FILE="/etc/varnish/secret"
 
+if [ -z "$STORAGE_FILE_PATH" ]; then
+    STORAGE_FILE_PATH="/var/lib/varnish"
+fi
+
+if [ -z "$STORAGE_FILE_SIZE" ]; then
+    STORAGE_FILE_SIZE="1G"
+fi
+
 [ -z "$ACL_PURGE_HOST" ] && ACL_PURGE_HOST="127.0.0.1"
 
 [ -n "$BACKEND_HOST" ] && sed -i "s/.*.host\ \=\ .*/.host\ \=\ \"$BACKEND_HOST\";/" $CONF_FILE
 [ -n "$BACKEND_PORT" ] && sed -i "s/.*.port\ \=\ .*/.port\ \=\ \"$BACKEND_PORT\";/" $CONF_FILE
 [ -n "$ACL_PURGE_HOST" ] && sed -i "s/.*acl_host.*/\"$ACL_PURGE_HOST\";/" $CONF_FILE
 
-varnishd -f $CONF_FILE -S $SECRET_FILE -F
+
+varnishd -f $CONF_FILE \
+    -S $SECRET_FILE -F \
+    -s file,$STORAGE_FILE_PATH/varnish_storage.bin,$STORAGE_FILE_SIZE
